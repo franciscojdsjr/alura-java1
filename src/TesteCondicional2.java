56afc14875867c5f99aa9cd3a8c8ef9a6da3ac0d
Francisco Junior <franciscojdsjr@gmail.com>
2021-03-08 20:52:51 -0300

    aula5

--- src/TesteCondicional2.java
+++ src/TesteCondicional2.java
@@ -5,18 +5,12 @@ public class TestaCondicional {
         System.out.println("testando condicionais");
 
         int idade = 16;
-        int quantidadePEsosas = 3;
+        int quantidadePesosas = 3;
 
-        if (idade >= 18) {
+        if (idade >= 18 || quantidadePesosas >= 2) {
 
             System.out.println("Você tem mais de 18 anos");
         } else {
-            if (quantidadePEsosas >= 2) {
-
-                System.out.println("Você não tem 18 mas pode entrar, mas você está acompanhado de " + quantidadePEsosas
-                        + " pessoas.");
-            }
-
             System.out.println("Infelizmente você não pode entrar");
         }
 
