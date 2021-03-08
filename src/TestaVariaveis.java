2c2b37247dae4e49ed3051c684b4c4f34b20069d
Francisco Junior <franciscojdsjr@gmail.com>
2021-03-08 17:49:14 -0300

    aula4

--- src/TestaVariaveis.java
+++ src/TestaVariaveis.java
@@ -1,9 +1,13 @@
-import jdk.internal.joptsimple.internal.Strings;
 
 public class TestaVariaveis {
 
     public static void main(String[] args) {
 
         System.out.println("ola novo teste");
+
+        int idade;
+        idade = 27;
+
+        System.out.println(idade);
     }
 }
