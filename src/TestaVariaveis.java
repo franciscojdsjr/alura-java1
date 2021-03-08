7293453c50e9b8d000da935232229204896675f2
Francisco Junior <franciscojdsjr@gmail.com>
2021-03-08 17:42:35 -0300

    aula4

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+import jdk.internal.joptsimple.internal.Strings;
+
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+
+        System.out.println("ola novo teste");
+    }
+}
