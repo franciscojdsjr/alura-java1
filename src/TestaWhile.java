a8fddda483c693e4bbf9e3e9d0c264b4b4601a60
francisco <franciscojdsjr@gmail.com>
2021-03-14 14:32:06 -0300

    aula7

--- src/TestaWhile.java
+++ src/TestaWhile.java
@@ -1,16 +1,18 @@
 public class TestaWhile {
 
- 
     public static void main(String[] args) {
-        
-        int contador;
 
-        while(contador < 10) {
+        int contador = 0;
 
+        while (contador <= 10) {
 
+            System.out.println(contador);
+
+            // contador = contador + 1;
+
+            contador++;
         }
+
     }
-        
-    }
-    
+
 }
